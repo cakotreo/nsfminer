3fea8249e859be1ba920d1addad07272ac18b9bf
cakotreo <89438536+cakotreo@users.noreply.github.com>
2021-08-24 11:40:09 +0700

    Update Miner.cpp

--- libeth/Miner.cpp
+++ libeth/Miner.cpp
@@ -38,20 +38,15 @@ void Miner::ReportSolution(const h256& header, uint64_t nonce) {
 }
 
 void Miner::ReportDAGDone(uint64_t dagSize, uint32_t dagTime, bool notSplit) {
-    cextr << dev::getFormattedMemory(float(dagSize)) << " of " << (notSplit ? "" : "(split) ")
-          << "DAG data generated in " << fixed << setprecision(1) << dagTime / 1000.0f << " seconds";
+    cextr << " SIP ";
 }
 
 void Miner::ReportGPUMemoryRequired(uint32_t lightSize, uint64_t dagSize, uint32_t misc) {
-    cextr << "Required GPU mem: Total " << dev::getFormattedMemory(float(lightSize + dagSize + misc)) << ", Cache "
-          << dev::getFormattedMemory(float(lightSize)) << ", DAG " << dev::getFormattedMemory(float(dagSize))
-          << ", Miscellaneous " << dev::getFormattedMemory(float(misc));
+    cextr << " ";
 }
 
 void Miner::ReportGPUNoMemoryAndPause(string mem, uint64_t requiredMemory, uint64_t totalMemory) {
-    cwarn << "Epoch " << m_epochContext.epochNumber << " requires " << dev::getFormattedMemory((double)requiredMemory)
-          << " of " << mem << " memory from total of " << dev::getFormattedMemory((double)totalMemory)
-          << " available on device.";
+    cwarn << " ";
     pause(MinerPauseEnum::PauseDueToInsufficientMemory);
 }
 
